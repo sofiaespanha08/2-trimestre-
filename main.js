a0b2c338263c210cb4e0db99b01055cb8e6bc83f
sofiaespanha08 <129747191+sofiaespanha08@users.noreply.github.com>
2024-07-01 08:54:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+const caixaPricipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoresultados = document.querySelector(".texto-resultados");
+
+const perguntas = [
+    {
+    enunciado: "pergunta 1",
+    alternativas: ["alternativa 1",
+        "alternativa 2"]
+    }
+
+{enunciado:"Pergunta 2",
+alternativas:[ "alternativa 1",
+"alternativa 2"]
+},
+]
